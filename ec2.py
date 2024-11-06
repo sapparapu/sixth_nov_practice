543f002ca37c629e40efccb4cfb16724b363ac19
sapparapu <sushma.ora08@gmail.com>
2024-11-06 13:32:12 +0530

    py file created

---
+++ ec2.py
@@ -0,0 +1,18 @@
+import boto3
+
+# Initialize a session using your AWS credentials
+ec2 = boto3.resource('ec2')
+
+# Create an EC2 instance
+instances = ec2.create_instances(
+    ImageId='ami-0c55b159cbfafe1f0',  # Replace with a valid AMI ID (e.g., Amazon Linux 2 AMI)
+    MinCount=1,  # Minimum number of instances
+    MaxCount=1,  # Maximum number of instances
+    InstanceType='t2.micro',  # Instance type (e.g., t2.micro)
+    KeyName='your-key-pair',  # Replace with your key pair name
+    SecurityGroupIds=['sg-xxxxxxxx'],  # Replace with your security group ID
+    SubnetId='subnet-xxxxxxxx',  # Replace with your subnet ID
+)
+
+print(f'Created instance with ID: {instances[0].id}')
+
